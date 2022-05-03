71b8479e5eb0de741835def2c40731b3ecd9d37f
Shubham <ssp671995@gmail.com>
2022-05-03 23:01:47 +0530

    Added refector On Save Create Employee Payroll Object.

--- js/FromValidation.js
+++ js/FromValidation.js
@@ -30,3 +30,57 @@ function salaryRange(){
         output.textContent = salary.value;
     });
 }
+
+/** On form submit populate employee payroll data object */
+const save = () => {
+    try{
+        let employeePayrollData = createEmployeePayroll();
+
+    }
+    catch(e){
+        return;
+    }
+    alert(employeePayrollData.toString());
+}
+
+const createEmployeePayroll = () => {
+    let employeePayrollData = new EmployeePayrollData();
+    try {
+        employeePayrollData.name = getInputValueById('#name');
+    } catch (e) {
+        setTextValue('.text-error', e);
+        throw e;
+    }
+
+    employeePayrollData.profilePic = getSelectedValues('[name=profile]').pop();
+    employeePayrollData.gender = getSelectedValues('[name=gender]').pop();
+    employeePayrollData.department = getSelectedValues('[name=department]');
+    employeePayrollData.salary = getInputValueById('#salary');
+    employeePayrollData.note = getInputValueById('#notes');
+    let date = getInputValueById('#day') + " " + getInputValueById('#month') + " " + getInputValueById('#year');
+        employeePayrollData.startDate = new Date(Date.parse(date));
+        setTextValue('.date-error', e)
+    employeePayrollData.date= Date.parse(date);
+    alert(employeePayrollData.toString());
+    return employeePayrollData;
+}
+
+const getSelectedValues = (propertyValue) => {
+    let allItems = document.querySelectorAll(propertyValue);
+    let setItems = [];
+    allItems.forEach(item => {
+        if (item.checked == true)
+            setItems.push(item.value);
+    });
+    return setItems;
+}
+
+const getInputValueById = (id) => {
+    let value = document.querySelector(id).value;
+    return value;
+}
+
+const getInputElementValue=(id)=>{
+    let value=document.getElementById(id).value;
+    return value;
+}
